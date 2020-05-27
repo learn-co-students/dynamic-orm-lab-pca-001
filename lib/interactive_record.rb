978b5bc0f1539f4aa9f6aa46dd0c28eed974d587
Pat McClernan <patmccler@gmail.com>
2020-05-27 17:55:15 -0400

    tests passing

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,69 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require "pry"
 
 class InteractiveRecord
-  
+
+  class << self
+    def table_name
+      to_s.downcase.pluralize
+    end
+
+    def column_names
+      DB[:conn].results_as_hash = true
+
+      sql = "pragma table_info('#{table_name}')"
+      DB[:conn].execute(sql).map{ |col| col["name"] }
+    end
+  end
+
+  def initialize(args={})
+    args.each do |k,v|
+      send("#{k}=", v) if respond_to? "#{k}=".to_sym
+    end
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.reject { |col| col == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    instance_variables.map do |var|
+      var = instance_variable_get(var)
+      var.nil? ? var : "\'#{var}\'"
+    end.compact.join(", ")
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+      VALUES (#{values_for_insert})
+    SQL
+
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM #{table_name}
+    WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name)
+  end
+
+  def self.find_by(attr)
+    sql = <<-SQL
+    SELECT * FROM #{table_name}
+    WHERE #{attr.first.first} = \"#{attr.first.last}\"
+    SQL
+
+    DB[:conn].execute(sql)
+
+  end
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,8 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require "pry"
 
 class Student < InteractiveRecord
-
+  column_names.each { |col| send(:attr_accessor, col) }
 end
